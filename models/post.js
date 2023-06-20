081200f38d8be45b68e326b567c1c0023a9e66f4
Kushal Jha <info.alonelynerd@gmail.com>
2023-06-20 16:43:08 +0530

    Added Comments to a Post.

--- models/post.js
+++ models/post.js
@@ -9,7 +9,14 @@ const postSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-    }
+    },
+    // Include the array of ids of all comments in this post Schema itself
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
   {
     timestamps: true,
--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,6 @@ router.use("/users", require("./users"));
 
 router.use("/posts", require("./posts"));
 
+router.use("/comments", require("./comments"));
+
 module.exports = router;
