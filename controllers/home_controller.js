8dc2f3f110c3f905e38c308748ad434f00b2313d
Kushal Jha <info.alonelynerd@gmail.com>
2023-06-21 19:02:50 +0530

    Added async-await to some of the files of controllers folder.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,22 +1,25 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-module.exports.home = (req, res) => {
-  Post.find({})
-    .populate("user")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "user",
-      },
-    })
-    .exec((err, posts) => {
-      User.find({}, (err, users) => {
-        return res.render("home", {
-          title: "Codezera | Home",
-          posts: posts,
-          all_users: users,
-        });
+module.exports.home = async (req, res) => {
+  try {
+    let posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
       });
+
+    let users = await User.find({});
+
+    return res.render("home", {
+      title: "Codezera | Home",
+      posts: posts,
+      all_users: users,
     });
+  } catch (err) {
+    console.log("Error!! ", err);
+  }
 };
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,31 +1,30 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
 
-module.exports.create = (req, res) => {
-  Post.create(
-    {
+module.exports.create = async (req, res) => {
+  try {
+    await Post.create({
       content: req.body.content,
       user: req.user._id,
-    },
-    (err, post) => {
-      if (err) {
-        return console.log("Error creating post");
-      }
-      return res.redirect("back");
-    }
-  );
+    });
+
+    return res.redirect("back");
+  } catch (err) {
+    console.log("Error in creating a new post ", err);
+  }
 };
 
-module.exports.destroy = (req, res) => {
-  Post.findById(req.params.id, (err, post) => {
-    // .id means converting the object id (_id) into string format.
+module.exports.destroy = async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
     if (post.user == req.user.id) {
       post.remove();
-      Comment.deleteMany({ post: req.params.id }, (err) => {
-        return res.redirect("back");
-      });
+      await Comment.deleteMany({ post: req.params.id });
+      return res.redirect("back");
     } else {
       return res.redirect("back");
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
